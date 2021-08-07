3dfcdcfd16892bbe3379e3d9407323e3f4e3ce94
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2021-08-07 15:23:24 +0100

    [llvm-9][layout] Added json input to llc

--- llvm/lib/Target/AArch64/AArch64AsmPrinter.cpp
+++ llvm/lib/Target/AArch64/AArch64AsmPrinter.cpp
@@ -1192,37 +1192,35 @@ void AArch64AsmPrinter::EmitInstruction(const MachineInstr *MI) {
 
   if (MI->isCall()) {
 
-	  OutContext.setAllowTemporaryLabels(true);
+	  OutContext.setAllowTemporaryLabels(true); // TODO check if both necessary
 	  OutContext.setUseNamesOnTempLabels(true);
 	  MCSymbol *MILabel = OutContext.createTempSymbol(MF->getName(), true);
-	  OutStreamer->EmitLabel(MILabel);
 
-	  outs() << MF->getName() << "\n";
-	  outs() << MILabel->getName() << "\n";
+	  StringRef Filename = TM.Options.MCOptions.CallsitePaddingFilename;
 
-	  ErrorOr<std::unique_ptr<MemoryBuffer>> FileOrErr = 
-		  MemoryBuffer::getFileOrSTDIN(TM.Options.MCOptions.CallsitePaddingFilename);
-	  if (std::error_code EC = FileOrErr.getError()) {
-		  auto Err = SMDiagnostic(CallsitePaddingFilename, SourceMgr::DK_Error,
-				  "Could not open input file: " + EC.message());
-	  }
-	  outs() << (FileOrErr.get())->getBuffer() << "\n";
+	  if (!Filename.empty()) {
 
-	  // Array and Object also have typed indexing accessors for easy traversal:
-	  //Expected<Value> E = parse(R"( {"options": {"font": "sans-serif"}} )");
-	  Expected<llvm::json::Value> E = llvm::json::parse((FileOrErr.get())->getBuffer());
-	  assert(E);
-	  Optional<int64_t> Padding;
-	  if (llvm::json::Object* O = E->getAsObject())
-		  if (llvm::json::Object* Opts = O->getObject("aarch64"))
-			  if (Padding = Opts->getInteger(MILabel->getName())) {
-				  assert(Opts->get(MILabel->getName())->kind() == llvm::json::Value::Number);
-			  }
+		  ErrorOr<std::unique_ptr<MemoryBuffer>> FileOrErr = 
+			  MemoryBuffer::getFileOrSTDIN(Filename);
+		  if (std::error_code EC = FileOrErr.getError()) {
+			  auto Err = SMDiagnostic(Filename, SourceMgr::DK_Error,
+					  "Could not open input file: " + EC.message());
+		  }
 
-	  if (Padding.hasValue()) {
-		  assert(Padding.getValue() % 4 == 0);
-		  for (int8_t I = 0; I < Padding.getValue() / 4; I++)
-			EmitToStreamer(*OutStreamer, MCInstBuilder(AArch64::HINT).addImm(0));
+		  Expected<llvm::json::Value> E = llvm::json::parse((FileOrErr.get())->getBuffer());
+		  assert(E);
+		  Optional<int64_t> Padding;
+		  if (llvm::json::Object* O = E->getAsObject())
+			  if (llvm::json::Object* Opts = O->getObject("aarch64"))
+				  if (Padding = Opts->getInteger(MILabel->getName())) {
+					  assert(Opts->get(MILabel->getName())->kind() == llvm::json::Value::Number);
+				  }
+
+		  if (Padding.hasValue()) {
+			  assert(Padding.getValue() % 4 == 0);
+			  for (int8_t I = 0; I < Padding.getValue() / 4; I++)
+				  EmitToStreamer(*OutStreamer, MCInstBuilder(AArch64::HINT).addImm(0));
+		  }
 	  }
   }
 
