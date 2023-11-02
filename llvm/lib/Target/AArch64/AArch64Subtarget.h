21186fea5b269a0830f2e8f4a8ae124be6aa9ae8
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2023-11-02 14:16:27 +0000

    [AArch64][ISel] Do now allow tricks with the csinc-like instructions
    These tricks avoid materializing constants like 1 or -1, by replacing
    them with the zero register and exploiting the semantics of csinc-like
    instructions. However, X86 doesn't support this, leading to different
    architecture-specific live values. Therefore, we add a flag to disallow
    this.
    
    Use with `-mattr=-csinc-tricks`.

--- llvm/lib/Target/AArch64/AArch64Subtarget.h
+++ llvm/lib/Target/AArch64/AArch64Subtarget.h
@@ -145,6 +145,7 @@ protected:
   bool HasX86FabsFneg = false;
   bool HasX86Select = false;
   bool Has64MovImm32 = false;
+  bool HasCSINCTricks = false;
 
   // Arm SVE2 extensions
   bool HasSVE2AES = false;
@@ -404,6 +405,7 @@ public:
   bool hasX86FabsFneg() const { return HasX86FabsFneg; }
   bool hasX86Select() const { return HasX86Select; }
   bool has64MovImm32() const { return Has64MovImm32; }
+  bool hasCSINCTricks() const { return HasCSINCTricks; }
   // Arm SVE2 extensions
   bool hasSVE2AES() const { return HasSVE2AES; }
   bool hasSVE2SM4() const { return HasSVE2SM4; }
